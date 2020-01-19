39cffcd45bf85808a25ac33e9bfc98c86d03b8bb
BeVAle <vlad.rotkopf@gmail.com>
2020-01-19 19:30:00 +0300

    ready file upload

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.Static("/", "./public")
+	router.POST("/upload", func(c *gin.Context) {
+		//name := c.PostForm("name")
+		name := "name"
+		//email := c.PostForm("email")
+		email := "email"
+
+		// Source
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+			return
+		}
+
+		filename := filepath.Base(file.Filename)
+		c.String(http.StatusOK,fmt.Sprintf("nuka: %s","./uploads/"+filename))
+		if err := c.SaveUploadedFile(file,"./uploads/"+filename); err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+			return
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf(" File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, name, email))
+	})
+	router.Run(":8080")
+}
